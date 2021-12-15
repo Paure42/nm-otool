7d12bfb055cc3a8c07298538a8528df2875ce0a5
Paure <paure@student.42lyon.fr>
2021-12-15 14:51:01 +0100

    done some checks in elf file and header

--- sources/main.c
+++ sources/main.c
@@ -9,13 +9,73 @@
  int g_buf_size;
  char *g_buffer;
 
-void m_read_file(const char *file, const char *ptr, const struct stat *statbuf)
+static inline int m_check_elf(const char *file, const char *ptr)
+{
+  switch ((unsigned char)ptr[EI_DATA]) {
+  case ELFDATA2LSB :
+    break;
+  case  ELFDATA2MSB :
+    break;
+  default :
+    o_error(file, "Incorrect elf data encoding");
+    return (-1);
+  } // check for data encoding, ie littlen endian or big endian
+
+  switch((unsigned char)ptr[EI_VERSION]) {
+  case  EV_CURRENT :
+    break;
+  default :
+    o_error(file, "Incorrect elf verion");
+    return (-1);
+  } // check for elf version
+
+  switch ((unsigned char)ptr[EI_OSABI]) {
+    case ELFOSABI_SYSV :
+      break;
+    case ELFOSABI_HPUX :
+      break;
+    case ELFOSABI_NETBSD :
+      break;
+    case ELFOSABI_SOLARIS :
+      break;
+    case ELFOSABI_IRIX :
+      break;
+    case ELFOSABI_FREEBSD :
+      break;
+    case ELFOSABI_TRU64 :
+      break;
+    case ELFOSABI_ARM :
+      break;
+    case ELFOSABI_STANDALONE :
+      break;
+    default :
+      o_error(file, "Incore elf ABI");
+  } // check for elf ABI
+  switch(((Elf64_Ehdr*)ptr)->e_type) {
+    case ET_REL :
+      break;
+    case ET_EXEC :
+      break;
+    case ET_DYN :
+      break;
+    case ET_CORE :
+      break;
+  default :
+    o_error(file, "Incorrect elf type");
+    return (-1);
+  } // check for elf type
+  return (0);
+}
+
+static inline void m_read_file(const char *file, const char *ptr, const struct stat *statbuf)
 {
   if ((unsigned char)ptr[EI_MAG0] == 0x7f &&
       (unsigned char)ptr[EI_MAG1] == 'E' &&
 	  (unsigned char)ptr[EI_MAG2] == 'L' &&
 	  (unsigned char)ptr[EI_MAG3] == 'F') {
 	// if the file is an elf
+	if (m_check_elf(file, ptr) == -1)
+    return;
 	bzero(g_buffer, g_buf_size); // TODO REPLACE WITH CUSTOM BZERO
 	switch ((unsigned char)ptr[EI_CLASS]) {
 	case ELFCLASS64: // if elf is 64 bits
@@ -24,15 +84,15 @@ void m_read_file(const char *file, const char *ptr, const struct stat *statbuf)
 	case ELFCLASS32: // if elf is 32 bits
 	  r_elf32(ptr);
 	  break;
-	case ELFCLASSNONE: // error ?
-	  o_error(file, "Incorrect elf type");
-	  break;
+  default:
+    o_error(file, "Incorrect elf type");
+    break;
 	}
   }
   (void)statbuf;
 }
 
-int m_open(const char *file) {
+static inline int m_open(const char *file) {
   int fd;
   char *ptr;
   struct stat statbuf;
@@ -80,6 +140,7 @@ int main(int argc, char *argv[])
 			m_open(argv[i]);
       o_success();
 		}
-	} // loop through all files in argv
+	} // loop through all files in arg$
+  free(g_buffer);
 	return (EXIT_SUCCESS);
 }
--- sources/r_elf64.c
+++ sources/r_elf64.c
@@ -5,7 +5,70 @@
 extern int  g_buf_size;
 extern char *g_buffer;
 
-static inline void get_names(const void *ptr, const Elf64_Shdr *sections, const Elf64_Sym *symtab, int i) // get symbols names
+static inline int s_elf64_machine(const char *file, const void *ptr)
+{
+  switch(((Elf64_Ehdr*)ptr)->e_machine) {
+  case EM_M32 :
+    break;
+  case EM_SPARC :
+    break;
+  case EM_386 :
+    break;
+  case EM_68K :
+    break;
+  case EM_88K :
+    break;
+  case EM_860 :
+    break;
+  case EM_MIPS :
+    break;
+  case EM_PARISC :
+    break;
+  case EM_SPARC32PLUS :
+    break;
+  case  EM_PPC :
+    break;
+  case EM_PPC64 :
+    break;
+  case EM_S390 :
+    break;
+  case EM_ARM :
+    break;
+  case EM_SH :
+    break;
+  case EM_SPARCV9 :
+    break;
+  case EM_IA_64 :
+    break;
+  case EM_X86_64 :
+    break;
+  default :
+         o_error(file, "Incorrect elf machine");
+         return (-1);
+    }
+  return (0);
+}
+
+static inline int s_elf64_header_values(const char *file, const void *ptr)
+{
+  switch (((Elf64_Ehdr*)ptr)->e_shnum) {
+    case SHN_LORESERVE :
+      o_error(file, "Incorrect entries number in the section header table");
+      return(-1);
+    case 0 :
+      o_error(file, "Incorrect entries number in the section header table");
+      return(-1);
+  }
+  return (0);
+}
+
+static inline void get_symbols_values(const Elf64_Sym *symtab, int i)
+  {
+    Elf64_Addr value = symtab[i].st_value;
+    snprintf((g_buffer + strlen(g_buffer)), (g_buf_size - strlen(g_buffer)), "value : %ld ", value);
+  }
+
+static inline void get_symbols_names(const void *ptr, const Elf64_Shdr *sections, const Elf64_Sym *symtab, int i) // get symbols names
 {
   char *strtab= (char *)(ptr + sections[sections[i].sh_link].sh_offset);
   for (size_t j = 0; j < (sections[i].sh_size/sections[i].sh_entsize); j++)
@@ -16,6 +79,10 @@ static inline void get_names(const void *ptr, const Elf64_Shdr *sections, const 
 
 void r_elf64(const void *ptr, const char *file)
 {
+  if (s_elf64_machine(file, ptr) == -1)
+    return;
+  if (s_elf64_header_values(file, ptr) == -1)
+    return;
   Elf64_Ehdr *header = (Elf64_Ehdr*)ptr; // header in elf64 of ptr
   Elf64_Shdr *sections = (Elf64_Shdr*)((char*)ptr + header->e_shoff); // locate the section table in the header
   Elf64_Sym *symtab = NULL;
@@ -24,8 +91,13 @@ void r_elf64(const void *ptr, const char *file)
       if (sections[i].sh_type == SHT_SYMTAB)
         {
           symtab = (Elf64_Sym*)((char*)ptr + sections[i].sh_offset); // locate the symbols table in the sections
-          get_names(ptr, sections, symtab, i);
-          break;
+          printf("SH_SIZE : %ld\nSH_ENTSIZE : %ld\nE_SHENTSIZE : %d\n", sections[i].sh_size, sections[i].sh_entsize, header->e_shentsize);
+          if (sections[i].sh_entsize == header->e_shentsize)
+            {
+              get_symbols_values(symtab, i);
+              get_symbols_names(ptr, sections, symtab, i);
+              break;
+            }
         }
     } // TODO sanity checking to make sure that none of the
       // indexes are out of range for the section they are
