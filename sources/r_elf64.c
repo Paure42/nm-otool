bbe55a4a20c9717ae30a00dd0682be81121fa0e9
Paure <paure@student.42lyon.fr>
2021-12-16 17:02:20 +0100

    get symbols of dynamic and hashes tables

--- sources/r_elf64.c
+++ sources/r_elf64.c
@@ -1,4 +1,5 @@
 #include "../includes/r_elf64.h"
+#include <elf.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -68,49 +69,66 @@ static inline void get_symbols_values(const Elf64_Sym *symtab, int j)
     snprintf((g_buffer + strlen(g_buffer)), (g_buf_size - strlen(g_buffer)), "value : %ld ", value);
   }
 
-static inline void get_symbols_names(const void *ptr, const Elf64_Shdr *sections, const Elf64_Sym *symtab, int i, int j)
+static inline void get_symbols_names(const void *ptr, const Elf64_Shdr *section_header, const Elf64_Sym *symtab, int i, int j)
   {
-    char *strtab= (char *)(ptr + sections[sections[i].sh_link].sh_offset);
+    char *strtab = (char *)(ptr + section_header[section_header[i].sh_link].sh_offset);
     snprintf((g_buffer + strlen(g_buffer)), (g_buf_size - strlen(g_buffer)), "name : %s\n", strtab + symtab[j].st_name);
   }
 
-static inline void get_symbols_attributes(const void *ptr, const Elf64_Shdr *sections, const Elf64_Sym *symtab, int i) // get symbols names
+static inline void get_symbols_sections(const void *ptr, const Elf64_Shdr *section_header, const Elf64_Sym *symtab, int j, int i)
 {
-  for (size_t j = 0; j < (sections[i].sh_size/sections[i].sh_entsize); j++)
+  char *strtab= (char *)(ptr + section_header[section_header[i].sh_link].sh_offset);
+  printf("SECTION HEADER NAME : %s\n", strtab + section_header[symtab[j].st_shndx].sh_name);
+}
+
+static inline void get_symbols_attributes(const void *ptr, const Elf64_Shdr *section_header, const Elf64_Sym *symtab, int i)
+{
+  for (size_t j = 0; j < (section_header[i].sh_size/section_header[i].sh_entsize); j++)
     {
       if (symtab[j].st_size > 0)
       {
         get_symbols_values(symtab, j);
-        get_symbols_names(ptr, sections, symtab, i, j);
+        get_symbols_names(ptr, section_header, symtab, i, j);
+        /* get_symbols_sections(ptr, section_header, symtab, j, i); */
       }
     }
 }
-
 void r_elf64(const void *ptr, const char *file)
 {
   if (s_elf64_machine(file, ptr) == -1)
     return;
   if (s_elf64_header_values(file, ptr) == -1)
     return;
-  Elf64_Ehdr *header = (Elf64_Ehdr*)ptr; // header in elf64 of ptr
-  Elf64_Shdr *sections = (Elf64_Shdr*)((char*)ptr + header->e_shoff); // locate the section table in the header
-  Elf64_Sym *symtab = NULL;
+  Elf64_Ehdr *header = (Elf64_Ehdr*)ptr; // header of elf64 file
+  Elf64_Shdr *section_header = (Elf64_Shdr*)((char*)ptr + header->e_shoff); // locate the section header
+  /* Elf64_Phdr *prog_header = ((Elf64_Phdr*)((char*)ptr + header->e_phoff)); // locate the program header */
   for (int i = 0; i < header->e_shnum; i++)
     {
-      if (sections[i].sh_type == SHT_SYMTAB)
+      if (section_header[i].sh_type == SHT_SYMTAB)
         {
-          symtab = (Elf64_Sym*)((char*)ptr + sections[i].sh_offset); // locate the symbols table in the sections
-          get_symbols_attributes(ptr, sections, symtab, i);
-          break;
+          Elf64_Sym *symtab = NULL;
+          symtab = (Elf64_Sym*)((char*)ptr + section_header[i].sh_offset); // locate the symbols table in the sections
+          get_symbols_attributes(ptr, section_header, symtab, i);
+        }
+      else if (section_header[i].sh_type == SHT_DYNSYM)
+        {
+          Elf64_Sym *symtab = NULL;
+          symtab = (Elf64_Sym*)((char*)ptr + section_header[i].sh_offset); // locate the symbols table in the sections
+          get_symbols_attributes(ptr, section_header, symtab, i);
+        }
+      else if (section_header[i].sh_type == SHT_HASH)
+        {
+          Elf64_Sym *symtab = NULL;
+          symtab = (Elf64_Sym*)((char*)ptr + section_header[i].sh_offset); // locate the symbols table in the sections
+          get_symbols_attributes(ptr, section_header, symtab, i);
         }
     } // TODO sanity checking to make sure that none of the
       // indexes are out of range for the section they are
       // indexing into, and that sh_entsize and and e_shentsize
       // match the sizeof the structs you are using, just in case the ELF file has been corrupted
-  if (symtab == NULL)
+  if (strlen(g_buffer) == 0)
     {
       snprintf(g_buffer, g_buf_size, "%s : no symbols", file);
       return;
     } // if there are no symbols in the table
-  printf("there is %ld symbols in the table\n", sizeof(symtab));
 }
